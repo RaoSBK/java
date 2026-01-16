7631b9acaf08d169d1d70e642d7f572285d7dc98
Suraj Bhan Kumar <surajbhan20005@gmail.com>
2026-01-16 11:46:20 +0530

    Wrraping up constants

--- practice.java
+++ practice.java
@@ -1,14 +1,56 @@
 import java.io.*;
+import java.util.*;
 
 public class main {
     public static void main (String[] args) {
-        String Fname= "Suraj Bhan";
-        String Lname = "Kumar";
-        System.out.println(Fname.length()); // find the length with the help of dot operator
-        System.out.println(Fname + " " + Lname); // concatinating 
-        System.out.println(Fname.charAt(4)); //print the letter from string with the help of index value
-        String name = Fname.replace('a', 'k'); // replacing the specific function with the help of specific character 
-        System.out.println(name);
-        System.out.println(Fname.substring(0, 6));
+        // String Fname= "Suraj Bhan";
+        // String Lname = "Kumar";
+        // System.out.println(Fname.length()); // find the length with the help of dot operator
+        // System.out.println(Fname + " " + Lname); // concatinating 
+        // System.out.println(Fname.charAt(4)); //print the letter from string with the help of index value
+        // String name = Fname.replace('a', 'k'); // replacing the specific function with the help of specific character 
+        // System.out.println(name);
+        // System.out.println(Fname.substring(0, 6)); // returnign a sub part of string
+
+
+        // //learning premitive data type
+        // int physics = 75;
+        // int chem = 84;
+        // int mathematics = 98;
+
+        // int[] marks = new int[3];
+        // marks[0] = 97;
+        // marks[1] = 98;
+        // marks[2]= 95;
+
+        // System.out.println(marks[1]);
+        
+
+
+        //Array
+        // System.out.println(marks[0]);
+        // Arrays.sort(marks);
+        // System.out.println(marks[0]);
+
+
+        // 2d array
+        // int[] [] finalMarks = {{97,94,99},{88,87,89}};
+        // System.out.println(finalMarks[1][2]);
+
+
+        // casting (converting variable form one type to another )
+        // double price = 100.00;
+        // double finalPrice = price +18;
+        // System.out.println(finalPrice);
+
+        // int p = 100;
+        // int fp = p + (int)18.0;
+        // System.out.println(fp);
+
+        //Constants
+        final float pi = 3.145F;
+        
+        System.out.println(pi);
+
     }
 }
