5aa2ec45d0f026009a952a801f95f0101207e431
Suraj Bhan Kumar <surajbhan20005@gmail.com>
2026-02-19 22:47:02 +0530

    completing polymorphism

--- oops.java
+++ oops.java
@@ -1,72 +1,103 @@
-class Pen{
-    String color;
-    String type;
+// class Pen{
+//     String color;
+//     String type;
 
-    public void write(){
-        System.out.println("Writing something ");
-    }
-    public void printColor(){
-        System.out.println(this.color);
-    }
-}
+//     public void write(){
+//         System.out.println("Writing something ");
+//     }
+//     public void printColor(){
+//         System.out.println(this.color);
+//     }
+// }
+
+
+// class Student{
+//     String name;
+//     int age;
+
+//     public void printInfo(){
+//         System.out.println(this.name);
+//         System.out.println(this.age);
+//     }
+
+//     // Student(String name, int age){
+//     //     this.name = name;
+//     //     this.age= age;
+//     // }
+
+//     Student(Student s2){
+//         this.name = s2.name;
+//         this.age = s2.age;
+//     }
+
+//     Student (){
+
+//     }
+// }
+
+// public class oops{
+//     public static void main(String args[]){
+
+
+//         // Pen pen1 = new Pen();
+//         // pen1.color = "Blue";
+//         // pen1.type = "Fountain Pen";
+
+//         // pen1.write();
+//         // pen1.printColor();
+//         // // System.out.println("Colour of the pen: "+pen1.color);
+//         // // System.out.println("Type of the pen: "+pen1.type);
+
+//         // Pen pen2 = new Pen();
+//         // pen2.color= "Black";
+//         // pen2.type = "gel";
+
+
+//         // pen1.printColor();
+//         // pen2.printColor();
+//         // pen2.write();
+
+//         // Student st1 = new Student("Suraj Bhan", 19);
+//         // st1.printInfo();
+
+//         Student st1 = new Student();
+//         st1.name = "Suraj";
+//         st1.age = 19;
+
+
+//         Student s2 = new Student(st1);
+//         s2.printInfo();
+
+//     }
+// }
+
+
+//polymorphism 
 
 
 class Student{
     String name;
     int age;
 
-    public void printInfo(){
-        System.out.println(this.name);
-        System.out.println(this.age);
+    public void printInfo(String name){
+        System.out.println(name);
     }
 
-    // Student(String name, int age){
-    //     this.name = name;
-    //     this.age= age;
-    // }
-
-    Student(Student s2){
-        this.name = s2.name;
-        this.age = s2.age;
+    public void printInfo(int age){
+        System.out.println(age);
     }
 
-    Student (){
-        
+    public void printInfo(String name, int age){
+        System.out.println(name+" "+ age);
     }
 }
 
 public class oops{
     public static void main(String args[]){
-
-
-        // Pen pen1 = new Pen();
-        // pen1.color = "Blue";
-        // pen1.type = "Fountain Pen";
-
-        // pen1.write();
-        // pen1.printColor();
-        // // System.out.println("Colour of the pen: "+pen1.color);
-        // // System.out.println("Type of the pen: "+pen1.type);
-
-        // Pen pen2 = new Pen();
-        // pen2.color= "Black";
-        // pen2.type = "gel";
-
-
-        // pen1.printColor();
-        // pen2.printColor();
-        // pen2.write();
-
-        // Student st1 = new Student("Suraj Bhan", 19);
-        // st1.printInfo();
-
-        Student st1 = new Student();
-        st1.name = "Suraj";
-        st1.age = 19;
-
-
-        Student s2 = new Student(st1);
-        s2.printInfo();
-
+        Student s1 = new Student();
+        s1.name = "Suraj Bhan";
+        s1.age = 19;
+        s1.printInfo(s1.name, s1.age);
+        
     }
 }
