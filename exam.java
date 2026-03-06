60fc5298494a15ba146e4dd8a20d701f8207ec25
Suraj Bhan Kumar <surajbhan20005@gmail.com>
2026-03-06 23:33:53 +0530

    Revising try, catch and finally block statements

--- exam.java
+++ exam.java
@@ -40,53 +40,120 @@
 
 
 // Finding the second largest number 
-import java.util.Scanner;
+// import java.util.Scanner;
 
-public class SecondLargestFinder {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+// public class SecondLargestFinder {
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
 
-        System.out.print("Enter the number of elements in the array: ");
-        int n = sc.nextInt();
+//         System.out.print("Enter the number of elements in the array: ");
+//         int n = sc.nextInt();
 
-        int[] arr = new int[n];
-        System.out.println("Enter " + n + " numbers:");
-        for (int i = 0; i < n; i++) {
-            arr[i] = sc.nextInt();
-        }
+//         int[] arr = new int[n];
+//         System.out.println("Enter " + n + " numbers:");
+//         for (int i = 0; i < n; i++) {
+//             arr[i] = sc.nextInt();
+//         }
 
-        int largest = arr[0];
-        for (int num : arr) {
-            if (num > largest) {
-                largest = num;
-            }
-        }
+//         int largest = arr[0];
+//         for (int num : arr) {
+//             if (num > largest) {
+//                 largest = num;
+//             }
+//         }
 
-        int secondLargest = Integer.MIN_VALUE;
-        for (int num : arr) {
-            if (num != largest && num > secondLargest) {
-                secondLargest = num;
-            }
-        }
+//         int secondLargest = Integer.MIN_VALUE;
+//         for (int num : arr) {
+//             if (num != largest && num > secondLargest) {
+//                 secondLargest = num;
+//             }
+//         }
 
-        int count = 0;
-        StringBuilder locations = new StringBuilder();
-        for (int i = 0; i < n; i++) {
-            if (arr[i] == secondLargest) {
-                count++;
-                locations.append(i).append(" "); // store index
-            }
-        }
+//         int count = 0;
+//         StringBuilder locations = new StringBuilder();
+//         for (int i = 0; i < n; i++) {
+//             if (arr[i] == secondLargest) {
+//                 count++;
+//                 locations.append(i).append(" "); // store index
+//             }
+//         }
 
         
-        if (secondLargest == Integer.MIN_VALUE) {
-            System.out.println("No second largest number found.");
-        } else {
-            System.out.println("Second largest number: " + secondLargest);
-            System.out.println("It appears " + count + " times.");
-            System.out.println("Locations (index): " + locations.toString());
+//         if (secondLargest == Integer.MIN_VALUE) {
+//             System.out.println("No second largest number found.");
+//         } else {
+//             System.out.println("Second largest number: " + secondLargest);
+//             System.out.println("It appears " + count + " times.");
+//             System.out.println("Locations (index): " + locations.toString());
+//         }
+
+//         sc.close();
+//     }
+// }
+
+
+
+import java.util.Scanner;
+
+public class tryCatchFinally{
+    public static void main(String args[]){
+        
+        try{
+            
+            Scanner sc = new Scanner(System.in);
+            int num1=0;
+            int num2=0;
+            int n;
+
+            n=0;
+            while (n < 3){
+
+                System.out.println("Enter the first number which is an even number");
+                num1 = sc.nextInt();
+                if(num1 % 2 == 0){
+                    System.out.println("Your enterd input is "+num1);
+                    break;
+                } else {
+                    
+                    if(n<3){
+                        System.out.println("Try to enter even number only");
+                    } else {
+                        System.out.println("Too many wrong attempts are enterd ");
+                        return;
+                    }
+                    
+                }
+                n++;
+            }
+
+            n=0;
+            while(n<3){
+                System.out.println("Enter the second number which is an odd number");
+                num2 = sc.nextInt();
+                if(num2 % 2 != 0){
+                    System.out.println("Your input is "+ num2);
+                    break;
+                } else {
+                    n++;
+                    if(n<3){
+                        System.out.println("Try again, Enter an odd number");
+                    } else {
+                        System.out.println("Too many wrong attempts");
+                    }
+                }
+            }
+            
+
+            int result = num1/num2;
+            System.out.println("Result: "+result);
+            sc.close();
+
+        } catch (ArithmeticException e){
+            System.out.println("An Exception occured"+e.getMessage());
+        } finally {
+            System.out.println("The finally block is always executed");
         }
 
-        sc.close();
+        System.out.println("Program continues after the try-catch-finally block.");
     }
 }
