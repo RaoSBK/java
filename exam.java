114781a2bbf07a9a75ae2f61b6c2f6a73ab44931
Suraj Bhan Kumar <surajbhan20005@gmail.com>
2026-03-07 23:28:10 +0530

    revising interfaces and classes

--- exam.java
+++ exam.java
@@ -93,67 +93,92 @@
 
 
 
-import java.util.Scanner;
+// import java.util.Scanner;
 
-public class tryCatchFinally{
-    public static void main(String args[]){
+// public class tryCatchFinally{
+//     public static void main(String args[]){
         
-        try{
+//         try{
             
-            Scanner sc = new Scanner(System.in);
-            int num1=0;
-            int num2=0;
-            int n;
+//             Scanner sc = new Scanner(System.in);
+//             int num1=0;
+//             int num2=0;
+//             int n;
 
-            n=0;
-            while (n < 3){
+//             n=0;
+//             while (n < 3){
 
-                System.out.println("Enter the first number which is an even number");
-                num1 = sc.nextInt();
-                if(num1 % 2 == 0){
-                    System.out.println("Your enterd input is "+num1);
-                    break;
-                } else {
+//                 System.out.println("Enter the first number which is an even number");
+//                 num1 = sc.nextInt();
+//                 if(num1 % 2 == 0){
+//                     System.out.println("Your enterd input is "+num1);
+//                     break;
+//                 } else {
                     
-                    if(n<3){
-                        System.out.println("Try to enter even number only");
-                    } else {
-                        System.out.println("Too many wrong attempts are enterd ");
-                        return;
-                    }
+//                     if(n<3){
+//                         System.out.println("Try to enter even number only");
+//                     } else {
+//                         System.out.println("Too many wrong attempts are enterd ");
+//                         return;
+//                     }
                     
-                }
-                n++;
-            }
+//                 }
+//                 n++;
+//             }
 
-            n=0;
-            while(n<3){
-                System.out.println("Enter the second number which is an odd number");
-                num2 = sc.nextInt();
-                if(num2 % 2 != 0){
-                    System.out.println("Your input is "+ num2);
-                    break;
-                } else {
-                    n++;
-                    if(n<3){
-                        System.out.println("Try again, Enter an odd number");
-                    } else {
-                        System.out.println("Too many wrong attempts");
-                    }
-                }
-            }
+//             n=0;
+//             while(n<3){
+//                 System.out.println("Enter the second number which is an odd number");
+//                 num2 = sc.nextInt();
+//                 if(num2 % 2 != 0){
+//                     System.out.println("Your input is "+ num2);
+//                     break;
+//                 } else {
+//                     n++;
+//                     if(n<3){
+//                         System.out.println("Try again, Enter an odd number");
+//                     } else {
+//                         System.out.println("Too many wrong attempts");
+//                     }
+//                 }
+//             }
             
 
-            int result = num1/num2;
-            System.out.println("Result: "+result);
-            sc.close();
+//             int result = num1/num2;
+//             System.out.println("Result: "+result);
+//             sc.close();
 
-        } catch (ArithmeticException e){
-            System.out.println("An Exception occured"+e.getMessage());
-        } finally {
-            System.out.println("The finally block is always executed");
-        }
+//         } catch (ArithmeticException e){
+//             System.out.println("An Exception occured"+e.getMessage());
+//         } finally {
+//             System.out.println("The finally block is always executed");
+//         }
 
-        System.out.println("Program continues after the try-catch-finally block.");
+//         System.out.println("Program continues after the try-catch-finally block.");
+//     }
+// }
+
+
+
+// Step 1: Create Interface
+interface Add {
+    void sum(int a, int b);
+}
+
+// Step 2: Implement Interface
+class Calculation implements Add {
+
+    public void sum(int a, int b) {
+        int result = a + b;
+        System.out.println("Sum = " + result);
+    }
+}
+
+// Step 3: Main Class
+public class exam {
+    public static void main(String[] args) {
+
+        Add obj = new Calculation();   // Interface reference
+        obj.sum(10, 20);
     }
 }
