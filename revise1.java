ed71f9342952651f66f5eea963dedcdcaf304c42
Suraj Bhan Kumar <surajbhan20005@gmail.com>
2026-03-20 23:38:11 +0530

    Revising loops and compile time polymorphisim

--- revise1.java
+++ revise1.java
@@ -34,49 +34,115 @@
 // }
 
 
-// Q2 Write a Java program using abstract class to perform:
-abstract class Shape{
-    abstract void draw();
-    void display(){
-        System.out.println("Displaying the shape");
-    }
+// // Q2 Write a Java program using abstract class to perform:
+// abstract class Shape{
+//     abstract void draw();
+//     void display(){
+//         System.out.println("Displaying the shape");
+//     }
+// }
+
+// class Circle extends Shape{
+//     @Override
+//     void draw(){
+//         System.out.println("Drawing a Cricle");
+//     }
+// }
+
+// class Rectangle extends Shape{
+//     @Override
+//     void draw(){
+//         System.out.println("Drawing a Triangle");
+//     }
+// }
+
+// class Triangle extends Shape{
+//     @Override
+//     void draw(){
+//         System.out.println("Drawind a Triangle");
+//     }
+// }
+
+// public class revise1{
+//     public static void main(String[] args){
+//         Shape s;
+
+//         s= new Circle();
+//         s.draw();
+//         s.display();
+
+//         s= new Rectangle();
+//         s.draw();
+//         s.display();
+
+//         s = new Triangle();
+//         s.draw();
+//         s.display();
+//     }
+// }
+
+
+// Write a Java program to: ,Take n elements in an array, Find sum of even numbers, Also count how many even numbers are present
+// Ans:- 
+// import java.util.Scanner;
+
+// public class revise1{
+//     public static void main(String[] args){
+//         Scanner sc = new Scanner(System.in);
+
+//         System.out.println("Enter number of elements: ");
+//         int n = sc.nextInt();
+
+//         int[] arr = new int[n];
+//         int sum = 0;
+//         int count = 0;
+
+//         System.out.println("Enter "+ n + "Elements: ");
+//         for(int i=0; i<n; i++){
+//             arr[i] = sc.nextInt();
+//             if(arr[i] %2 == 0){
+//                 sum += arr[i];
+//                 count++;
+//             }
+//         }
+
+//         System.out.println("Sum of even numbers: "+ sum);
+//         System.out.println("Count of even numbers: "+ count);
+
+//         sc.close();
+//     }
+// }
+
+// Write a Java program to implement compile-time polymorphism using interface.
+// Requirements:
+// Create interface Operation
+// Declare method sum()
+// Overload method sum(int, int) and sum(double, double)
+// Create class Calculation that implements interface
+// Call both methods in main()
+
+interface Operation{
+    void sum(int a, int b);
+    void sum(double a, double b);
 }
 
-class Circle extends Shape{
-    @Override
-    void draw(){
-        System.out.println("Drawing a Cricle");
+class Calculation implements Operation{
+    public void sum(int a, int b){
+        int result = a+b;
+        System.out.println("Sum of integers: "+ result);
     }
-}
 
-class Rectangle extends Shape{
-    @Override
-    void draw(){
-        System.out.println("Drawing a Triangle");
-    }
-}
-
-class Triangle extends Shape{
-    @Override
-    void draw(){
-        System.out.println("Drawind a Triangle");
+    public void sum(double a, double b){
+        double result = a+b;
+        System.out.println("Sum of doubles: "+result);
     }
 }
 
 public class revise1{
     public static void main(String[] args){
-        Shape s;
-
-        s= new Circle();
-        s.draw();
-        s.display();
-
-        s= new Rectangle();
-        s.draw();
-        s.display();
-
-        s = new Triangle();
-        s.draw();
-        s.display();
+        Calculation calc = new Calculation();
+        
+        calc.sum(10, 20);
+        calc.sum(15.5, 24.5);
     }
 }
